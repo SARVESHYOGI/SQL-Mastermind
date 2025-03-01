37754b73262063eca258ce7fd9225c776886159e
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2025-03-02 00:19:24 +0530

    frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        {/* <Button variant="primary">Click Me</Button> */}
+        hi
+      </div>
+    </>
+  )
+}
+
+export default App
